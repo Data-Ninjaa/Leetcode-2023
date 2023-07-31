b43b581e7f0396291d27c2f8bbf196e469410884
Haleema Sadia <95963370+Data-Ninjaa@users.noreply.github.com>
2023-07-31 16:54:45 +0500

    Updated Running_sum1480.py

--- Running_sum1480.py
+++ Running_sum1480.py
@@ -1,17 +1,13 @@
 #Leetcode problem 1480
 #Running Sum of 1D Array
+#method runnig_sum
 def running_sum(nums):
+    # loop to iterate from 2nd index of the nums list
     for i in range(1,len(nums)):
+        #updating the nums list
         nums[i] = nums[i-1]+nums[i]
+    #return the updated list
     return nums
-def runningSum(nums):
-    n = len(nums)
-    result = [0] * n
-    result[0] = nums[0]
-    for i in range(1, n):
-        result[i] = result[i-1] + nums[i]
-    return result
-
 nums = [1,2,3,4,5]
-new_array = runningSum(nums)
+new_array = running_sum(nums)
 print(new_array)
